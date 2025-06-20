e1b09e8ac2796d2bfcb666f7d876ac71a5b05507
Animesh Sharma <animesh.sharma2@cognizant.com>
2025-06-20 16:55:40 +0000

    Initial Commit @6/20/2025, 4:55:40 PM

---
+++ node-tut/app.js
@@ -0,0 +1,6 @@
+module.exports={
+    x:10,
+    y:20
+}
+// export var x=10;
+// export var y=10;
---
+++ node-tut/index.js
@@ -0,0 +1,4 @@
+var app = require('./app')
+// import {x} from './app'
+
+console.log(app.x, app.y)
